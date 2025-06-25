576e2931ad54338444782ac0f190fdd1b5760e64
viannik <ivan.nikitski@gmail.com>
2025-06-25 21:51:34 +0300

    Add cinema management classes and functionality

---
+++ app/cinema/hall.py
@@ -0,0 +1,22 @@
+from typing import List
+
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
+
+
+class CinemaHall:
+
+    def __init__(self, number: int) -> None:
+        self.number = number
+
+    def movie_session(
+            self,
+            movie_name: str,
+            customers: List[Customer],
+            cleaning_staff: Cleaner
+    ) -> None:
+        print(f"\"{movie_name}\" started in hall number {self.number}.")
+        for customer in customers:
+            customer.watch_movie(movie=movie_name)
+        print(f"\"{movie_name}\" ended.")
+        cleaning_staff.clean_hall(hall_number=self.number)
--- app/main.py
+++ app/main.py
@@ -1,6 +1,33 @@
-# write your imports here
+from typing import List, Dict
+
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(
+        customers: List[Dict[str, str]],
+        hall_number: int,
+        cleaner: str,
+        movie: str
+) -> None:
+    customer_objects = [
+        Customer(name=customer["name"], food=customer.get("food"))
+        for customer in customers
+    ]
+    cinema_hall = CinemaHall(number=hall_number)
+    cleaner_object = Cleaner(name=cleaner)
+
+    for customer in customer_objects:
+        if customer.food:
+            CinemaBar.sell_product(
+                product=customer.food,
+                customer=customer
+            )
+
+    cinema_hall.movie_session(
+        movie_name=movie,
+        customers=customer_objects,
+        cleaning_staff=cleaner_object
+    )
